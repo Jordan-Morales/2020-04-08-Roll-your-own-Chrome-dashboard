0f33b03013a58590aebebcbdc3876243c132726f
Jordan Morales <jordan.morales@gmail.com>
2020-04-08 12:53:11 -0500

    random background pulled from unsplash, random quote pulled from quote garden

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('hello world.')
+
+
+// jquery container doc ready
+$(() => {
+
+
+  // new background with each new tab
+  // rnadom
+  $("html").css({
+    "background-image": "url('https://source.unsplash.com/random/3840x2160')",
+    "background-repeat": "no-repeat",
+    "background-size": "cover"
+  });
+
+  let quote = "";
+  let author = "";
+
+  $.get("https://quote-garden.herokuapp.com/api/v2/quotes/random", (data) => {
+    quote = data.quote.quoteText;
+    author = data.quote.quoteAuthor;
+    let newQuote = $('<p>' + quote + '</p>' + '<h3>' + author + '</h3>');
+    $(".retrievedQuote").html(newQuote);
+  })
+
+
+
+
+})
